399eb12bb115f0f9c1cde41265f5672c886f8e27
m8loot <tazorbeatsss@gmail.com>
2023-10-26 16:18:03 +0300

    fix arrays

--- src/store/todosSlice.ts
+++ src/store/todosSlice.ts
@@ -16,7 +16,7 @@ const todosSlice = createSlice({
   } as initialState,
   reducers: {
     addTasks: (state, action) => {
-      state.tasks.push({
+      state.tasks?.push({
         id: Date.now(),
         title: action.payload,
         favorite: false,
@@ -26,13 +26,13 @@ const todosSlice = createSlice({
       localStorage.setItem("tasks", JSON.stringify(state.tasks));
     },
     editTask: (state, action) => {
-      state.tasks.map((task) => {
+      state.tasks?.map((task) => {
         if (task.id === action.payload.id) task.title = action.payload.title;
       });
       localStorage.setItem("tasks", JSON.stringify(state.tasks));
     },
     deleteTask: (state, action) => {
-      state.tasks = state.tasks.filter((task) => task.id !== action.payload);
+      state.tasks = state.tasks?.filter((task) => task.id !== action.payload);
       localStorage.setItem("tasks", JSON.stringify(state.tasks));
     },
     setAddFormToggle: (state) => {
@@ -41,19 +41,19 @@ const todosSlice = createSlice({
     },
     setEditToggle: (state, action) => {
       state.editToggle = !state.editToggle;
-      state.tasks.map((task) => {
+      state.tasks?.map((task) => {
         if (task.id === action.payload) task.edit = !task.edit;
       });
       localStorage.setItem("tasks", JSON.stringify(state.tasks));
     },
     setFavorite: (state, action) => {
-      state.tasks.map((task) => {
+      state.tasks?.map((task) => {
         if (task.id === action.payload) task.favorite = !task.favorite;
       });
       localStorage.setItem("tasks", JSON.stringify(state.tasks));
     },
     setComplet: (state, action) => {
-      state.tasks.map((task) => {
+      state.tasks?.map((task) => {
         if (task.id === action.payload) task.checked = !task.checked;
       });
       localStorage.setItem("tasks", JSON.stringify(state.tasks));
